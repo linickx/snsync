ae070aa9d0684173564c82a3990e858f4e994245
Nick <linickx@gmail.com>
2021-04-30 15:21:12 +0100

    Add Environment Variable Support

--- simplenote_sync/config.py
+++ simplenote_sync/config.py
@@ -42,24 +42,74 @@ class Config:
             cp.add_section(cfg_sec)
 
         self.configs = collections.OrderedDict()
-        self.configs['sn_username'] = [cp.get(cfg_sec, 'cfg_sn_username', raw=True), 'Simplenote Username']
-        self.configs['sn_password'] = [cp.get(cfg_sec, 'cfg_sn_password', raw=True), 'Simplenote Password']
-        self.configs['cfg_nt_ext'] = [cp.get(cfg_sec, 'cfg_nt_ext'), 'Note file extension']
-        self.configs['cfg_nt_path'] = [cp.get(cfg_sec, 'cfg_nt_path'), 'Note storage path']
-        self.configs['cfg_nt_trashpath'] = [cp.get(cfg_sec, 'cfg_nt_trashpath'), 'Note Trash Bin Folder for deleted notes']
-        self.configs['cfg_nt_filenamelen'] = [cp.get(cfg_sec, 'cfg_nt_filenamelen'), 'Length of Filename']
-        self.configs['cfg_log_level'] = [cp.get(cfg_sec, 'cfg_log_level'), 'snsync log level']
+
+        #
+        #    Environment Varialbles over-ride config file settings.
+        #    Config files are cfg_abc
+        #    Envs are sn_abc
+        #
+
+        if os.environ.get('sn_username') is None:
+            val_sn_username = cp.get(cfg_sec, 'cfg_sn_username', raw=True)
+        else:
+            val_sn_username = os.environ.get('sn_username')
+        self.configs['sn_username'] = [val_sn_username, 'Simplenote Username']
+
+        if os.environ.get('sn_password') is None:
+            val_sn_passowrd = cp.get(cfg_sec, 'cfg_sn_password', raw=True)
+        else:
+            val_sn_passowrd = os.environ.get('sn_password')
+        self.configs['sn_password'] = [val_sn_passowrd, 'Simplenote Password']
+
+        if os.environ.get('sn_nt_ext') is None:
+            val_sn_nt_ext = cp.get(cfg_sec, 'cfg_nt_ext')
+        else:
+            val_sn_nt_ext = os.environ.get('sn_nt_ext')
+        self.configs['cfg_nt_ext'] = [val_sn_nt_ext, 'Note file extension']
+
+        if os.environ.get('sn_nt_path') is None:
+            val_sn_nt_path = cp.get(cfg_sec, 'cfg_nt_path')
+        else:
+            val_sn_nt_path = os.environ.get('sn_nt_path')
+        self.configs['cfg_nt_path'] = [val_sn_nt_path, 'Note storage path']
+
+        if os.environ.get('sn_nt_trashpath') is None:
+            val_sn_nt_trashpath = cp.get(cfg_sec, 'cfg_nt_trashpath')
+        else:
+            val_sn_nt_trashpath = os.environ.get('sn_nt_trashpath')
+        self.configs['cfg_nt_trashpath'] = [val_sn_nt_trashpath, 'Note Trash Bin Folder for deleted notes']
+
+        if os.environ.get('sn_nt_filenamelen') is None:
+            val_sn_nt_filenamelen = cp.get(cfg_sec, 'cfg_nt_filenamelen')
+        else:
+            val_sn_nt_filenamelen = os.environ.get('sn_nt_filenamelen')
+        self.configs['cfg_nt_filenamelen'] = [val_sn_nt_filenamelen, 'Length of Filename']
+
+        if os.environ.get('sn_log_level') is None:
+            val_sn_log_level = cp.get(cfg_sec, 'cfg_log_level')
+        else:
+            val_sn_log_level = os.environ.get('sn_log_level')
+        self.configs['cfg_log_level'] = [val_sn_log_level, 'snsync log level']
 
         # Dynamic Defaults
-        if cp.has_option(cfg_sec, 'cfg_db_path'):
-            self.configs['cfg_db_path'] = [cp.get(cfg_sec, 'cfg_db_path'), 'snsync database location']
+        if os.environ.get('sn_db_path') is None:
+            if cp.has_option(cfg_sec, 'cfg_db_path'):
+                val_sn_db_path = cp.get(cfg_sec, 'cfg_db_path')
+            else:
+                val_sn_db_path = os.path.join(cp.get(cfg_sec, 'cfg_nt_path'), '.snsync.sqlite')
         else:
-            self.configs['cfg_db_path'] = [os.path.join(cp.get(cfg_sec, 'cfg_nt_path'), '.snsync.sqlite'), 'snsync database location']
+            val_sn_db_path = os.environ.get('sn_db_path')
+        self.configs['cfg_db_path'] = [val_sn_db_path, 'snsync database location']
 
-        if cp.has_option(cfg_sec, 'cfg_log_path'):
-            self.configs['cfg_log_path'] = [cp.get(cfg_sec, 'cfg_log_path'), 'snsync log location']
+        if os.environ.get('sn_log_path') is None:
+            if cp.has_option(cfg_sec, 'cfg_log_path'):
+                val_sn_log_path = cp.get(cfg_sec, 'cfg_log_path')
+            else:
+                val_sn_log_path = os.path.join(cp.get(cfg_sec, 'cfg_nt_path'), '.snsync.log')
         else:
-            self.configs['cfg_log_path'] = [os.path.join(cp.get(cfg_sec, 'cfg_nt_path'), '.snsync.log'), 'snsync log location']
+            val_sn_log_level = os.environ.get('sn_log_path')
+        self.configs['cfg_log_path'] = [val_sn_log_path, 'snsync log location']
+
 
     def get_config(self, name):
         """
